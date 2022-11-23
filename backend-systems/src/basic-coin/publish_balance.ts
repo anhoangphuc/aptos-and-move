094d191f98cec60ca9c7e17e27181b710e2fe592
hoangphuc <hoangphucnb97@gmail.com>
2022-11-23 17:41:58 +0700

    Add comment for pretty

--- backend-systems/src/basic-coin/publish_balance.ts
+++ backend-systems/src/basic-coin/publish_balance.ts
@@ -5,6 +5,7 @@ import path from "path";
 import { loadRandomAccount} from "../utils";
 
 (async function () {
+    //build payload
     const publishBalanceABIs = fs.readFileSync(path.join(__dirname, "./abis/publish_balance.abi"), "hex");
     const builder = new TransactionBuilderABI([new HexString(publishBalanceABIs).toUint8Array()])
     const payload = builder.buildTransactionPayload(
@@ -12,16 +13,24 @@ import { loadRandomAccount} from "../utils";
         [],
         [],
     );
+
+    // generate raw transaction
     const randomAccount = await loadRandomAccount();
     const rawTx = await client.generateRawTransaction(randomAccount.address(), payload);
+
+    // Check resource, not exist now
     const balanceResource = `${DEPLOYED_ADDRESS}::basic_coin::Balance`;
     console.log(`Random account is ${randomAccount.address()}`);
     let resources = await client.getAccountResources(randomAccount.address());
     let isExist = resources.some((resource) => resource.type === balanceResource);
     console.log(`Account ${randomAccount.address()} ${isExist ? 'has' : 'doesnt have'} ' resource`);
+
+    // Submit signed transaction
     const bcsTxn = AptosClient.generateBCSTransaction(randomAccount, rawTx);
     const transactionRes = await client.submitSignedBCSTransaction(bcsTxn);
     await client.waitForTransaction(transactionRes.hash);
+
+    // Check resource, exist now
     resources = await client.getAccountResources(randomAccount.address());
     isExist = resources.some((resource) => resource.type === balanceResource);
     console.log(`Account ${randomAccount.address()} ${isExist ? 'has' : 'doesnt have'} ' resource`);
